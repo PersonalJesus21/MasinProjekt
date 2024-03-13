3424248804a38953f115654900d8e8ecaba8e353
PersonalJesus21 <somr666@seznam.cz>
2024-03-13 12:35:07 +0100

    Movement Update / přidal jsem sprites

--- MasinProjekt/Assets/C#/PlayerMovement.cs
+++ MasinProjekt/Assets/C#/PlayerMovement.cs
@@ -1,24 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed;
+    private float horizontal;
+    private float speed = 3f;
+    private bool isFacingRight = true;
     public Rigidbody2D body;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update() {
-
-float xInput = Input.GetAxis("Horizontal");
-float yInput = Input.GetAxis("Vertical");
-Vector2 direction = new Vector2(xInput, yInput).normalized;
-body.velocity = direction * speed;
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        Flip();
+       
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
     }
 }
+
+    
+    
+    
+
+   
+
+
